1a3429284b1633ec726398c880a86aa15ea0524e
MyUsername <alberto.baglivi@gmail.com>
2022-10-20 17:04:49 +0200

    add the second version

--- js/script.js
+++ js/script.js
@@ -1,38 +1,74 @@
+const people1= {
 
-class People{
-    constructor(name,role,photo) {
-         this.name = name;
-         this.role = role;
-         this.photo = photo;
-       }
-   }
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    photo: "img/wayne-barnett-founder-ceo.jpg"
+};
 
-const people1 = new People("Wayne Barnett","Founder & CEO","img/wayne-barnett-founder-ceo.jpg");
-const people2 = new People("Angela Caroll","Chief Editor","img/angela-caroll-chief-editor.jpg");
-const people3 = new People("Walter Gordon","Office Manager","img/walter-gordon-office-manager.jpg");
-const people4 = new People("Angela Lopez","Social Media Manager","img/angela-lopez-social-media-manager.jpg");
-const people5 = new People("Scott Estrada","Developer","img/scott-estrada-developer.jpg");
-const people6 = new People("Barbara Ramos","Graphic Designer","img/barbara-ramos-graphic-designer.jpg");
+const people2= {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    photo: "img/angela-caroll-chief-editor.jpg"
+};
 
-const peopleTeams  = [];
+const people3= {
+    name:"Walter Gordon",
+    role: "Office Manager",
+    photo: "img/walter-gordon-office-manager.jpg"
+};
+
+const people4= {
+    name : "Angela Lopez",
+    role: "Social Media Manager",
+    photo: "img/angela-lopez-social-media-manager.jpg"
+};
+
+const people5= {
+    name: "Scott Estrada",
+    role: "Developer",
+    photo: "img/scott-estrada-developer.jpg"
+};
+
+const people6= {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    photo: "img/barbara-ramos-graphic-designer.jpg"
+};
 
 
+const peopleTeams = [people1,people2,people3,people4,people5,people6];
 
-
-
-peopleTeams.push(people1);
-peopleTeams.push(people2);
-peopleTeams.push(people3);
-peopleTeams.push(people4);
-peopleTeams.push(people5);
-peopleTeams.push(people6);
-
-console.log(peopleTeams);
 
 const list_team = document.getElementById("list_team");
 
-
 CreateTeam(peopleTeams); 
+
+
+// class People{
+//     constructor(name,role,photo) {
+//          this.name = name;
+//          this.role = role;
+//          this.photo = photo;
+//        }
+//    }
+
+// const people1 = new People("Wayne Barnett","Founder & CEO","img/wayne-barnett-founder-ceo.jpg");
+// const people2 = new People("Angela Caroll","Chief Editor","img/angela-caroll-chief-editor.jpg");
+// const people3 = new People("Walter Gordon","Office Manager","img/walter-gordon-office-manager.jpg");
+// const people4 = new People("Angela Lopez","Social Media Manager","img/angela-lopez-social-media-manager.jpg");
+// const people5 = new People("Scott Estrada","Developer","img/scott-estrada-developer.jpg");
+// const people6 = new People("Barbara Ramos","Graphic Designer","img/barbara-ramos-graphic-designer.jpg");
+
+// const peopleTeams  = [];
+
+// peopleTeams.push(people1);
+// peopleTeams.push(people2);
+// peopleTeams.push(people3);
+// peopleTeams.push(people4);
+// peopleTeams.push(people5);
+// peopleTeams.push(people6);
+
+// console.log(peopleTeams);
 
 
 
