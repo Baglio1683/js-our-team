248de9a3635de3a0512bd216c7c43584758ac00f
MyUsername <alberto.baglivi@gmail.com>
2022-10-20 16:31:10 +0200

    add the cards with picture

--- js/script.js
+++ js/script.js
@@ -5,17 +5,20 @@ class People{
          this.role = role;
          this.photo = photo;
        }
-   
    }
 
-const people1 = new People("Wayne Barnett","Founder & CEO","wayne-barnett-founder-ceo.jpg");
-const people2 = new People("Angela Caroll","Chief Editor","angela-caroll-chief-editor.jpg");
-const people3 = new People("Walter Gordon","Office Manager","walter-gordon-office-manager.jpg");
-const people4 = new People("Angela Lopez","Social Media Manager","angela-lopez-social-media-manager.jpg");
-const people5 = new People("Scott Estrada","Developer","scott-estrada-developer.jpg");
-const people6 = new People("Barbara Ramos","Graphic Designer","barbara-ramos-graphic-designer.jpg");
+const people1 = new People("Wayne Barnett","Founder & CEO","img/wayne-barnett-founder-ceo.jpg");
+const people2 = new People("Angela Caroll","Chief Editor","img/angela-caroll-chief-editor.jpg");
+const people3 = new People("Walter Gordon","Office Manager","img/walter-gordon-office-manager.jpg");
+const people4 = new People("Angela Lopez","Social Media Manager","img/angela-lopez-social-media-manager.jpg");
+const people5 = new People("Scott Estrada","Developer","img/scott-estrada-developer.jpg");
+const people6 = new People("Barbara Ramos","Graphic Designer","img/barbara-ramos-graphic-designer.jpg");
 
 const peopleTeams  = [];
+
+
+
+
 
 peopleTeams.push(people1);
 peopleTeams.push(people2);
@@ -41,10 +44,18 @@ function CreateTeam(arrayMembers){
 
     for(let j=0 ; j < arrayMembers.length ; j++){
 
-        let member = document.createElement('li'); 
+        let member = document.createElement('div');
+        member.classList.add("card"); 
+
+        let description = document.createElement('p'); 
+        description.innerHTML =  arrayMembers[j].name + ", " + arrayMembers[j].role; 
         
-        member.innerHTML = arrayMembers[j].name + ", " + arrayMembers[j].role + ", " + arrayMembers[j].photo; 
-     
+        let pic = document.createElement('img'); 
+        pic.src = arrayMembers[j].photo;
+
+        member.appendChild(pic)
+        member.appendChild(description); 
+
         list_team.append(member); 
      
      }
